e9e863c37b0b5314f455ac8cbfd342ed1f72b538
Majd Alkhdr <totallynotswann@gmail.com>
2024-04-17 07:02:46 -0500

    Completed the Neural Network and Created a DataLoader for training. used pyplot to graph the performance

--- deeplearner.py
+++ deeplearner.py
@@ -3,6 +3,23 @@ import torch.nn as nn
 import cv2 as cv
 import random
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
+
+images1 = ["faces/s1/1.pgm", "faces/s6/3.pgm", "faces/s30/7.pgm", "faces/s19/9.pgm", "faces/s2/4.pgm", "faces/s7/3.pgm"]
+images2 = ["faces/s1/9.pgm", "faces/s2/4.pgm", "faces/s4/3.pgm", "faces/s19/7.pgm", "faces/s3/4.pgm", "faces/s7/10.pgm"]
+labels = [1, 0, 0, 1, 0, 1]
+
+class SiameseDataset():
+    def __init__(self) -> None:
+        pass
+
+    def getDataSet():
+        randomSet = random.randint(0, len(labels) - 1)
+        img1 = cv.imread(images1[randomSet], cv.IMREAD_GRAYSCALE)
+        img2 = cv.imread(images2[randomSet], cv.IMREAD_GRAYSCALE)
+
+        return img1, img2, labels[randomSet]
+
 
 class SiameseNetwork(nn.Module):
     def __init__(self):
@@ -35,7 +52,6 @@ class SiameseNetwork(nn.Module):
     def forward_once(self, x):
         output = self.cnn(x)
         output = output.view(output.size()[0], -1)
-        print(output.size())
         output = self.fc(output)
         return output
 
@@ -57,28 +73,51 @@ class ContrastiveLoss(nn.Module):
 
 
 network = SiameseNetwork()
+data = SiameseDataset
 contrastive_loss = ContrastiveLoss()
 
 optimizer = torch.optim.Adam(network.parameters(), lr=0.001)
 
-img = cv.imread("faces/s1/1.pgm", cv.IMREAD_GRAYSCALE)
-img2 = cv.imread("faces/s1/2.pgm", cv.IMREAD_GRAYSCALE)
+num_epochs = int(input("How many epochs would you like to train?"))
 
-optimizer.zero_grad()
+negative_values = []
+positive_values = []
 
-img_tensor = torch.from_numpy(img).unsqueeze(0).unsqueeze(0).float()
-img_tensor2 = torch.from_numpy(img2).unsqueeze(0).unsqueeze(0).float()
-
-
-x = network.forward_once(img_tensor)
-y = network.forward_once(img_tensor2)
-
-label = torch.tensor([1])
-loss = contrastive_loss(x, y, label)
-
-loss.backward()
-optimizer.step()
-
-print(loss)
-
+for epoch in range(num_epochs):
     
+    img1, img2, label = data.getDataSet()
+
+    optimizer.zero_grad()
+
+    img_tensor = torch.from_numpy(img1).unsqueeze(0).unsqueeze(0).float()
+    img_tensor2 = torch.from_numpy(img2).unsqueeze(0).unsqueeze(0).float()
+
+
+    x = network.forward_once(img_tensor)
+    y = network.forward_once(img_tensor2)
+
+    label = torch.tensor([label])
+    loss = contrastive_loss(x, y, label)
+
+    loss.backward()
+    optimizer.step()
+    print(label.item(), loss.item())
+    if label.item() == 0:
+        negative_values.append(loss.item())
+    else:
+        positive_values.append(loss.item())
+        
+    
+
+plt.plot(negative_values, marker='o', linestyle='-')
+plt.grid(True)
+plt.title("Negative")
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+
+plt.plot(positive_values, marker='x', linestyle='-')
+plt.grid(True)
+plt.title("Negative")
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.show()
